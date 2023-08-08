2aef227921e92475073020e5844afccded21bb69
K4U3G4RC14 <kauegarcia666@gmai.com>
2023-08-08 20:40:52 -0300

    criação do codigo java script finalização do css

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const random=() =>{
+    const  min = Math.ceil(document.querySelector('.min').value)
+    const max = Math.floor(document.querySelector('.max').value)
+    const generetNumber =(Math.random() * (max - min + 1) + min).toFixed()
+
+    return alert(`O ganhador é ${generetNumber}`)
+}
+
+
+
 
